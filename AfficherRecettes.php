f13294359f1c6bf9b2a018825595e6dd22816eb7
AnaelleChiles <chiles.anaelle@gmail.com>
2023-09-06 11:38:33 +0200

    Les tableaux associatifs

--- AfficherRecettes.php
+++ AfficherRecettes.php
@@ -1,34 +1,67 @@
 <?php
-    $recipes = [
+    /*$recipes = [
         ['Cassoulet','[...]','mickael.andrieu@exemple.com',true,],
         ['Couscous','[...]','mickael.andrieu@exemple.com',false,],
-    ];
+    ];*/
+    $recipe['title'] = 'Cassoulet';
+    $recipe['recipe'] = 'Etape 1 : des flageolets, ...';
+    $recipe['author'] = 'john.doe@exemple.com';
+    $recipe['enable'] = true;
 ?>
 
 <!DOCTYPE html>
 <html>
     <head>
         <title>Affichage des recettes</title>
+        <link rel="stylesheet" type="text/css" href="style.css">
         <meta charset="utf-8" />
     </head>
     <body>
     <ul>
-        <?php for ($lines = 0; $lines <= 1; $lines++): ?>
-            <li>
-                <?php echo $recipes[$lines][0] . ' (' . $recipes[$lines][2] . ')';?>
-            </li>
-        <?php endfor; ?>
+    <?php
+        $recipes = [
+            [
+                'title' => 'Cassoulet',
+                'recipe' => '',
+                'author' => 'mickael.andrieu@exemple.com',
+                'is_enabled' => true,
+            ],
+            [
+                'title' => 'Couscous',
+                'recipe' => '',
+                'author' => 'mickael.andrieu@exemple.com',
+                'is_enabled' => false,
+            ],
+            [
+                'title' => 'Escalope milanaise',
+                'recipe' => '',
+                'author' => 'mathieu.nebra@exemple.com',
+                'is_enabled' => true,
+            ],
+            [
+                'title' => "Patate arc en ciel a la poudre d'escampette",
+                'recipe' => '',
+                'author' => 'jsp.monNom@gmal.com',
+                'is_enabled' => true,
+            ]
+        ];
+        
+        $isEnabled = true;
+        if ($isEnabled == true) {
+            echo "<h1><center> Affichage des recettes</center></h1>";
+            foreach($recipes as $recipe) {
+                echo "<h2>" . $recipe['title'] ."</h2>". $recipe['recipe']. ' recette : '. '<br>'  . $recipe['author'] . '<br>'. '<br>' ;   
+            }
+        }
+        else {
+            echo "Accès refusé !";
+        }
+        ?>
+
+
     </ul>
        
-        <?php/*
-            $isEnabled = true;
-            if ($isEnabled == true) {
-                echo "Accès autorisé !";
-            }
-            else {
-                echo "Accès refusé !";
-            }*/
-        ?>
+        
         
 
 
