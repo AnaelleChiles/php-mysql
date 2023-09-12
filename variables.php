8bfc626e7f92ee8f17f6391f85ecbc8c31a85397
AnaelleChiles <chiles.anaelle@gmail.com>
2023-09-12 16:11:14 +0200

    projet fil rouge

---
+++ variables.php
@@ -0,0 +1,50 @@
+<?php
+    $recipes = [
+        [
+            'title' => 'Cassoulet',
+            'recipe' => '',
+            'author' => 'mickael.andrieu@exemple.com',
+            'is_enabled' => true,
+        ],
+        [
+            'title' => 'Couscous',
+            'recipe' => '',
+            'author' => 'mickael.andrieu@exemple.com',
+            'is_enabled' => false,
+        ],
+        [
+            'title' => 'Escalope milanaise',
+            'recipe' => '',
+            'author' => 'mathieu.nebra@exemple.com',
+            'is_enabled' => false,
+        ],
+        [
+            'title' => "Patate arc en ciel a la poudre d'escampette",
+            'recipe' => '',
+            'author' => 'jsp.monNom@gmal.com',
+            'is_enabled' => true,
+        ]
+    ];
+		
+	$users = [
+        [
+            'mail' => 'mickael.andrieu@exemple.com',
+			'nom' => 'PATATE',
+			'prenom' => 'Douce',
+            'age' => 80,
+         ],
+        [
+            'mail' => 'mickael.andrieu@exemple.com',
+            'nom' => 'Sanglante',
+			'prenom' => 'Clementine',
+            'age' => 36,
+         ],
+        [
+            'mail' => 'mathieu.nebra@exemple.com',
+            'nom' => 'Pain',
+			'prenom' => 'Jean-mis',
+            'age' => 45,
+        ]
+    ];
+
+?>
