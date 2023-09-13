4daaa4942a6c678fd8effe4e02005914eac1c570
AnaelleChiles <chiles.anaelle@gmail.com>
2023-09-13 11:52:57 +0200

    conservez les donnees grace aux session version prof

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
+<?php session_start();?>
 <!DOCTYPE html>
 <html>
 
@@ -26,7 +27,7 @@
         <h1>Site de Recettes !</h1>
 
         <!-- Si l'utilisateur existe, on affiche les recettes -->
-        <?php if (isset($loggedUser)): ?>
+        <?php if (isset($_SESSION['LOGGED_USER'])): ?>
             <?php foreach (getRecipes($recipes) as $recipe): ?>
                 <article>
                     <h3>
--- login.php
+++ login.php
@@ -1,9 +1,10 @@
-<?php
+<?php  
 // Validation du formulaire
 if (isset($_POST['email']) && isset($_POST['password'])) {
     foreach ($users as $user) {
         if ($user['email']===$_POST['email'] && $user['password']===$_POST['password']) {
-            $loggedUser = ['email' => $user['email'],];
+            //$loggedUser = ['email' => $user['email'],];
+            $_SESSION['LOGGED_USER'] = $user['email'];
         } else {
             $errorMessage = sprintf('Les informations envoyées ne permettent pas de vous identifier : (%s/%s)', $_POST['email'], $_POST['password']);
         }
@@ -12,7 +13,7 @@ if (isset($_POST['email']) && isset($_POST['password'])) {
 ?>
 
 <!-- Si utilisateur/trice est non identifié(e), on affiche le formulaire -->
-<?php if (!isset($loggedUser)): ?>
+<?php if (!isset($_SESSION['LOGGED_USER'])): ?>
     <form action="index.php" method="post">
         <!-- si message d'erreur on l'affiche -->
         <?php if (isset($errorMessage)): ?>
@@ -37,6 +38,6 @@ if (isset($_POST['email']) && isset($_POST['password'])) {
 
     <!-- Si utilisateur/trice bien connectée on affiche un message de succès -->
 <?php else: ?>
-    <div class="alert alert-success" role="alert"> Bonjour <?php echo $loggedUser['email']; ?> et bienvenue sur le site !
+    <div class="alert alert-success" role="alert"> Bonjour <?php echo $_SESSION['LOGGED_USER']; ?> et bienvenue sur le site !
     </div>
 <?php endif; ?>
